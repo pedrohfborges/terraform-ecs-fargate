6d9e5a69e25de9ac4026e016a0c66fdfc5cbaa37
Pedro Henrique Ferreira Borges <111831621+pedrohfborges@users.noreply.github.com>
2024-08-01 09:33:30 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,30 +1,27 @@
 ####NET CONFIGURATION
 
       variable "vpc" {
-        default     = "192.168.0.0/16"
         description = "Set VCN IP"
       }
 
       variable "public_subnet" {
-        default     = "192.168.30.0/24"
         description = "Set Subnet Public"
       }
 
       variable "netprivate" {
-        default     = "192.168.40.0/24"
         description = "Set subnet Private"
       }
 
       variable "cluster_name" {
-        default = "NomedoClusterTeste"
+        description  = "NomedoClusterTeste"
       }
 
       variable "task_definition_family" {
-        default = "my-task-def" # Nome da definição de tarefa ECS
+        description = "Nome da Tarefa"
       }
 
       variable "service_name" {
-        default = "my-ecs-service" # Nome do serviço ECS
+        description = "Nome do servico"
       }
 
 

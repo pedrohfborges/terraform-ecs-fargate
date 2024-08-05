e9885ac5696fe77dd1319aed34e3326f8a6e606b
Pedro Henrique Ferreira Borges <111831621+pedrohfborges@users.noreply.github.com>
2024-08-05 10:58:12 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,30 +1,39 @@
 ####NET CONFIGURATION
 
-      variable "vpc" {
-        description = "Set VCN IP"
-      }
+variable "vpc" {
+  description = "Set VCN IP"
+}
 
-      variable "public_subnet" {
-        description = "Set Subnet Public"
-      }
+variable "public_subnet" {
+  description = "Set Subnet Public"
+}
 
-      variable "netprivate" {
-        description = "Set subnet Private"
-      }
+variable "netprivate" {
+  description = "Set subnet Private"
+}
 
-      variable "cluster_name" {
-        description  = "NomedoClusterTeste"
-      }
+variable "cluster_name" {
+  description = "NomedoClusterTeste"
+}
 
-      variable "task_definition_family" {
-        description = "Nome da Tarefa"
-      }
+variable "task_definition_family" {
+}
 
-      variable "service_name" {
-        description = "Nome do servico"
-      }
+variable "service_name" {
+}
 
+variable "nometarefa" {
+}
+variable "containerimage" {
+}
+variable "cpu" {
+}
 
+variable "memory" {
+}
+
+variable "containerport" {
+}
 
 
 
